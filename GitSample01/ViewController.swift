75b186c1eaeb07b98077f9666ce2c66a865ef4b1
Huang495-ling <20cm0112@jec.ac.jp>
2020-09-04 12:11:53 +0900

    app

---
+++ GitSample01/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GitSample01
+//
+//  Created by cmStudent on 2020/09/04.
+//  Copyright © 2020 20cmo112. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    print("Hello,Swift!")
+    }
+
+
+}
+
